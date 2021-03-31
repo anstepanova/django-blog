dde06621f94f0fe5ecf491be07b2678c100c7f2e
Anastasia Stepanova <anstepanova@griddynamics.com>
2021-03-31 12:08:58 +0400

    Tag is added

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
     slug = models.SlugField(max_length=150, unique=True)
     body = models.TextField(blank=True, db_index=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     date_pub = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
@@ -14,4 +15,17 @@ class Post(models.Model):
 
     def __repr__(self):
         return f'title:{self.title}, date_pub:{self.date_pub}, slug:{self.slug}'
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug':self.slug})
+
+    def __repr__(self):
+        return f'title:{self.title}, slug:{self.slug}'
+
+
 
