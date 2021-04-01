0006bef0cad420a0290fe96c25eb72783f7d82ee
Anastasia Stepanova <anstepanova@griddynamics.com>
2021-04-01 16:06:06 +0400

    Added the Mixin for creating posts and tags to avoid  code duplication

--- blog/utils.py
+++ blog/utils.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 
 from .models import Post, Tag
@@ -11,3 +11,19 @@ class ObjectDetailMixin:
     def get(self, request, slug):
         obj = get_object_or_404(self.model, slug__iexact=slug)
         return render(request, self.template, context={self.model.__name__.lower(): obj})
+
+
+class ObjectCreateMixin:
+    form = None
+    template = None
+
+    def get(self, request):
+        form = self.form()
+        return render(request, self.template, context={'form': form})
+
+    def post(self, request):
+        bound_form = self.form(request.POST)
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(request, 'blog/tag_create.html', context={'form': bound_form})
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.views.generic import View
 from django.shortcuts import get_object_or_404
 
 from .models import Post, Tag
-from .utils import ObjectDetailMixin
+from .utils import ObjectDetailMixin, ObjectCreateMixin
 from .forms import TagForm, PostForm
 
 
@@ -18,30 +18,14 @@ class TagDetail(ObjectDetailMixin, View):
     template = 'blog/tag_detail.html'
 
 
-class TagCreate(View):
-    def get(self, request):
-        form = TagForm()
-        return render(request, 'blog/tag_create.html', context={'form': form})
-
-    def post(self, request):
-        bound_form = TagForm(request.POST)
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-        return render(request, 'blog/tag_create.html', context={'form': bound_form})
+class TagCreate(ObjectCreateMixin, View):
+    form = TagForm
+    template = 'blog/tag_create.html'
 
 
-class PostCreate(View):
-    def get(self, request):
-        form = PostForm()
-        return render(request, 'blog/post_create.html', context={'form': form})
-
-    def post(self, request):
-        bound_form = PostForm(request.POST)
-        if bound_form.is_valid():
-            new_post = bound_form.save()
-            return redirect(new_post)
-        return render(request, 'blog/post_create.html', context={'form': bound_form})
+class PostCreate(ObjectCreateMixin, View):
+    form = PostForm
+    template = 'blog/post_create.html'
 
 
 def posts_list(request):
